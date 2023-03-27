ed42555784e63b16350e1465965a5222d6c46fbc
steve <981357560@qq.com>
2023-03-27 11:41:22 +0800

    unfinished method4

--- src/components/action_selectors.py
+++ src/components/action_selectors.py
@@ -66,3 +66,43 @@ class EpsilonGreedyActionSelector():
 
 
 REGISTRY["epsilon_greedy"] = EpsilonGreedyActionSelector
+
+
+class EpsilonGreedyMethod5ActionSelector():
+
+    def __init__(self, args):
+        self.args = args
+
+        self.schedule = DecayThenFlatSchedule(args.epsilon_start, args.epsilon_finish, args.epsilon_anneal_time,
+                                              decay="linear")
+        self.epsilon = self.schedule.eval(0)
+        self.selected_idxs = (th.arange(5, device=args.device).reshape(1, -1) + -2 + args.ucb).clip(0, 99)
+
+    def select_action(self, agent_inputs, avail_actions, t_env, test_mode=False):
+
+        # Assuming agent_inputs is a batch of Q-Values for each agent bav
+        self.epsilon = self.schedule.eval(t_env)
+
+        if test_mode:
+            # Greedy action selection only
+            self.epsilon = 0.0
+
+        # mask actions that are excluded from selection
+        masked_z_values = agent_inputs.clone()
+        if test_mode:
+            masked_q_values = masked_z_values.mean(dim=-1)
+        else:
+            qtls = th.gather(masked_z_values, 1, self.selected_idxs)
+            masked_q_values = (1 - self.args.weight) * masked_q_values.mean(dim=-1) + self.args.weight * qtls.mean(dim=-1)
+
+        masked_q_values[avail_actions == 0.0] = -float("inf")  # should never be selected!
+
+        random_numbers = th.rand_like(masked_q_values[:, :, 0])
+        pick_random = (random_numbers < self.epsilon).long()
+        random_actions = Categorical(avail_actions.float()).sample().long()
+
+        picked_actions = pick_random * random_actions + (1 - pick_random) * masked_q_values.max(dim=2)[1]
+        return picked_actions
+
+
+REGISTRY["method5"] = EpsilonGreedyActionSelector
